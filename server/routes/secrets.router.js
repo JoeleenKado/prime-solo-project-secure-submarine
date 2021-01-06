780684db407609c6202d4b611e6670f258e68c5d
joellekado <jkado@my.hpu.edu>
2021-01-05 18:25:36 -0600

    clearance level enforced in get secrets

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -6,11 +6,15 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 router.get('/', rejectUnauthenticated, (req, res) => {
   //this route should return all secrets
+
   console.log('in /api/secrets GET route');
   console.log('Is User logged in?', req.isAuthenticated());
   console.log('req.user:', req.user);
+  
+  let userClearance = req.user.clearance_level;
+  
   pool
-    .query('SELECT * FROM "secret";')
+    .query(`SELECT * FROM "secret" WHERE "secrecy_level" <= ${userClearance};`)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
